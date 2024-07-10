494e29caa23769ca5878eed7b47212e138da3016
@Nurul97 <nurulislamnextgenitltd.com>
2024-07-10 15:27:06 +0600

    Contact

---
+++ src/components/ContactCon.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function ContactCon() {
+  return (
+    <>
+      <div className="container">
+         <div className="row mt-4">
+            <div className="col-md-6">
+               <p className='text-justify'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit praesentium consectetur nobis dolorem, iure explicabo! Tempora, quisquam ab placeat accusantium voluptates harum quam nisi! Ipsum libero beatae, molestiae cupiditate corrupti optio fugiat consequuntur quis? Reiciendis vero veritatis quo dolorum laboriosam corporis, totam laborum. Corrupti amet eligendi, libero ut cupiditate nisi impedit eius fuga natus exercitationem, sint accusantium. Eos et, delectus quas iusto non, tenetur quidem est corrupti quasi earum dolore laboriosam fugit eum soluta dignissimos aliquam sequi esse doloribus fuga explicabo repellendus? Vitae natus sequi ipsum culpa numquam laudantium maxime molestiae placeat officiis architecto dolor repudiandae nisi, maiores, deleniti quisquam quod reiciendis officia pariatur commodi reprehenderit, earum dolorem odit alias nesciunt. Aliquam molestias quos iure facilis atque, quibusdam quam consequatur veniam ipsa labore ullam dolor. Officia earum culpa distinctio eligendi iure provident architecto possimus, fuga et. Porro quisquam blanditiis omnis asperiores dolores, maxime esse! Officia laboriosam minus quidem perspiciatis asperiores.</p>
+            </div>
+            <div className="col-md-6">
+               <p className='text-justify'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit praesentium consectetur nobis dolorem, iure explicabo! Tempora, quisquam ab placeat accusantium voluptates harum quam nisi! Ipsum libero beatae, molestiae cupiditate corrupti optio fugiat consequuntur quis? Reiciendis vero veritatis quo dolorum laboriosam corporis, totam laborum. Corrupti amet eligendi, libero ut cupiditate nisi impedit eius fuga natus exercitationem, sint accusantium. Eos et, delectus quas iusto non, tenetur quidem est corrupti quasi earum dolore laboriosam fugit eum soluta dignissimos aliquam sequi esse doloribus fuga explicabo repellendus? Vitae natus sequi ipsum culpa numquam laudantium maxime molestiae placeat officiis architecto dolor repudiandae nisi, maiores, deleniti quisquam quod reiciendis officia pariatur commodi reprehenderit, earum dolorem odit alias nesciunt. Aliquam molestias quos iure facilis atque, quibusdam quam consequatur veniam ipsa labore ullam dolor. Officia earum culpa distinctio eligendi iure provident architecto possimus, fuga et. Porro quisquam blanditiis omnis asperiores dolores, maxime esse! Officia laboriosam minus quidem perspiciatis asperiores.</p>
+            </div>
+         </div>
+      </div>
+    </>
+  )
+}
+
+export default ContactCon
